2863819bf1a9936d65cf29ddfab8fac61a31b269
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 10:40:20 +0000

    Exercise 1:for loop

---
+++ Argumentos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For Loop exercicio1 
+            for (int i = 0; i < args; i++)
+            {
+            Console.WriteLine(i);
+            }
+        }
+    }
+}
