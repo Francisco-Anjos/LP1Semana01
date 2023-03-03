67d10267915c1bb6efd7f9a5178b5690d7088ef7
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 11:01:35 +0000

    Exercise3:int

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variaveis inteiras");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
