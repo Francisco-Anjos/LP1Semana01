41742495dac6f7acedaaaacfc0033fcdb5277585
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 11:36:28 +0000

    Exercise3:Real

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -9,6 +9,8 @@ namespace TiposVarios
             char P ='\u0050';
             char p ='\u0070';
             char u ='\u0075';
+            float h =5.59f;
+            double j = 60.40e-40;
             Console.WriteLine("Variaveis inteiras");
             for (int i = 0; i < 6; i++)
             {
@@ -18,6 +20,9 @@ namespace TiposVarios
             Console.WriteLine(P + " " + "u0050");
             Console.WriteLine(p + " " + "u0070");
             Console.WriteLine(u + " " + "u0075");
+            Console.WriteLine("Reais");
+            Console.WriteLine(h);
+            Console.WriteLine(j);
         }
     }
 }
