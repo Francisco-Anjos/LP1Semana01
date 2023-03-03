5c00c7b570961f9d054a6656624374e5c6662c02
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 11:23:43 +0000

    Exercise3: Unicode

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,11 +6,18 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            char P ='\u0050';
+            char p ='\u0070';
+            char u ='\u0075';
             Console.WriteLine("Variaveis inteiras");
             for (int i = 0; i < 6; i++)
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine("Unicode");
+            Console.WriteLine(P + " " + "u0050");
+            Console.WriteLine(p + " " + "u0070");
+            Console.WriteLine(u + " " + "u0075");
         }
     }
 }
