9585c76750ba6340df3ec37cbd3b089ad83a7923
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 11:54:43 +0000

    Exercise4:Strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m = "This is a \u0050";
+            string n = "Accidentaly made a \u0070";
+            string b = @"varias strigs \u0075";
+            string c = @"Algumas strigs \u0055";
+
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
