672e9dcffeef67a0764f0de170314c594e1f5fc6
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-03 10:49:57 +0000

    Exercise2: Documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,10 +2,18 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Class for the program being ran 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main funcion to write the assigned words 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Bye world");
         }
